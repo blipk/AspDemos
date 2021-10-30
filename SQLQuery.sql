17ac8d515403ca58fdc44ed0a3e9bf67ddeb7883
K <45532845+blipk@users.noreply.github.com>
2021-10-30 12:55:41 +0000

    Delete SQLQuery.sql

--- SQLQuery.sql
+++
@@ -1,6 +0,0 @@
-﻿--insert into Employees(ApplicationUserId, EmployeeNumber, SoftDeleteLevel, CreatedDate, UpdatedDate) select ApplicationUserId, EmployeeNumber, SoftDeleteLevel, CreatedDate, UpdatedDate from Employees;
-
-UPDATE Employees SET SoftDeleteLevel = 0;
-UPDATE AspNetUsers SET SoftDeleteLevel = 0;
-SELECT * FROM Employees;
-SELECT * FROM AspNetUsers;
